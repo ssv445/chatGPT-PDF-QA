36ce57605584d3ba1117f09ac04fc1a3d8c35996
Shyam <shyam@readybytes.in>
2023-08-31 08:25:35 +0000

    ts err

--- utils/supabase-client.ts
+++ utils/supabase-client.ts
@@ -2,11 +2,11 @@ import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
 import { createClient } from "@supabase/supabase-js";
 
 
-const privateKey = process.env.SUPABASE_PRIVATE_KEY;
-if (!privateKey) throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
+const privateKey = process.env.SUPABASE_PRIVATE_KEY || '';
+if (privateKey == '') throw new Error(`Expected env var SUPABASE_PRIVATE_KEY`);
 
-const url = process.env.SUPABASE_URL;
-if (!url) throw new Error(`Expected env var SUPABASE_URL`);
+const url = process.env.SUPABASE_URL || '';
+if (url == '') throw new Error(`Expected env var SUPABASE_URL`);
 
 
 async function init() {
